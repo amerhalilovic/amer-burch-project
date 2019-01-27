6522027d55f9789ba4b748e9fb4735fdfcf1d104
amerhalilovic <amer.malibu@gmail.com>
2019-01-27 13:06:03 +0100

    styles

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path')
 const router = require('./routes.js')
 const app = express();
-
+const port = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://admin:admin123@ds213615.mlab.com:13615/projekat', {useNewUrlParser: true},function(err){
     if(err) {
@@ -19,4 +19,8 @@ app.use('/api', router)
 
 app.get('*', function(req, res){
     res.sendFile(path.join(__dirname + 'public/index.html'))
+})
+
+app.listen(port, function(){
+    console.log("Listening on " + port)
 })
