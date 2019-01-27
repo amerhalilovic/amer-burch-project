4996f6c498c3dc18d3e24d3554d4795c5e22ebf8
amerhalilovic <amer.malibu@gmail.com>
2019-01-27 12:23:44 +0100

    project structure

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path')
+const router = require('./routes.js')
+const app = express();
+
+
+mongoose.connect('mongodb://admin:admin123@ds213615.mlab.com:13615/projekat', {useNewUrlParser: true},function(err){
+    if(err) {
+        console.log('Some problem with the connection ' +err);
+    } else {
+        console.log('The Mongoose connection is ready');
+    }
+})
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/api', router)
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname + 'public/index.html'))
+})
